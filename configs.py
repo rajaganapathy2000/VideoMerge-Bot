4a0df02c414167ee2c4e50645c286ed753ec0261
Raja Ganapathy <79167443+rajaganapathy2000@users.noreply.github.com>
2021-07-18 17:57:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,15 +17,15 @@ class Config(object):
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1340559309))
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
+Hi , I am Video Merge Bot!
 I can Merge Multiple Videos in One Video. Video Formats should be same.
 
-Made by @AbirHasan2005
+Made by @SDFBots
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "Video Merged by @{}"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
