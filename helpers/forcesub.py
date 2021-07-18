e2bb228eae36d1b199bef5aa5c7e6101ba2be8e9
Raja Ganapathy <79167443+rajaganapathy2000@users.noreply.github.com>
2021-07-18 17:27:09 +0530

    Update forcesub.py

--- helpers/forcesub.py
+++ helpers/forcesub.py
@@ -21,7 +21,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/SDFBots).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -29,7 +29,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            text="**Please Join My Updates Group to use this Bot!**\n\nDue to Overload, Only Group Members can use the Bot!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -46,7 +46,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/SDFBots).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
