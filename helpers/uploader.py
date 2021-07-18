f0b4680f1a5d48810c18e4102ea16cc145e5aefa
Raja Ganapathy <79167443+rajaganapathy2000@users.noreply.github.com>
2021-07-18 17:31:36 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -30,9 +30,7 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                        [InlineKeyboardButton("Support Group", url="https://t.me/SDFBots")]
                     ]
                 )
             )
@@ -51,9 +49,7 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                       [InlineKeyboardButton("Support Group", url="https://t.me/SDFBots")]
                     ]
                 )
             )
